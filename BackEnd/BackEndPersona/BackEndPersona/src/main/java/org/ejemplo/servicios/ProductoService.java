cab97209d59d2d54d24b9fb3e42f3545276644f5
Brisa Panichelli <panichellibrisa@gmail.com>
2023-10-10 19:33:02 -0300

    logica de servicios para productos

--- BackEnd/BackEndPersona/BackEndPersona/src/main/java/org/ejemplo/servicios/ProductoService.java
+++ BackEnd/BackEndPersona/BackEndPersona/src/main/java/org/ejemplo/servicios/ProductoService.java
@@ -1,22 +1,67 @@
 package org.ejemplo.servicios;
 
 import org.ejemplo.modelos.Producto;
+import org.ejemplo.exceptions.ProductException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductoService {
 
-    public List<Producto> obtenerTodosLosProductos();
+    private List<Producto> productos = new ArrayList<>();
+    private final ProductoValidation productoValidation;
 
-    public Producto obtenerProductoPorId(int id);
+    @Autowired
+    public ProductoService(ProductoValidation productoValidation) {
+        this.productoValidation = productoValidation;
+    }
 
-    public Producto agregarProducto(Producto producto);
+    public List<Producto> obtenerTodosLosProductos() {
+        return productos;
+    }
 
-    public Producto actualizarProducto(int id, Producto producto);
+    public Producto obtenerProductoPorId(int id) {
+        for (Producto producto : productos) {
+            if (producto.getId() == id) {
+                return producto;
+            }
+        }
+        throw new ProductException("No se encontró el producto con el ID dado.");
+    }
 
-    void eliminarProducto(int id);
+    public Producto agregarProducto(Producto producto) {
+        if (productoValidation.validarProducto(producto)) {
+            producto.setId(generarIdUnico());
+            productos.add(producto);
+            return producto;
+        } else {
+            throw new ProductException("El producto no pasa las validaciones.");
+        }
+    }
 
-    public List<Producto> buscarProductosPorParametros(String nombre, Float precio, String marca, String modelo);
+    public Producto actualizarProducto(int id, Producto productoActualizado) {
+        Producto productoExistente = obtenerProductoPorId(id);
+        if (productoValidation.validarProducto(productoActualizado)) {
+            productoExistente.setNombre(productoActualizado.getNombre());
+            productoExistente.setDescripcion(productoActualizado.getDescripcion());
+            productoExistente.setPrecio(productoActualizado.getPrecio());
+            productoExistente.setMarca(productoActualizado.getMarca());
+            productoExistente.setModelo(productoActualizado.getModelo());
+            return productoExistente;
+        } else {
+            throw new ProductException("El producto actualizado no pasa las validaciones.");
+        }
+    }
+
+    public void eliminarProducto(int id) {
+        Producto producto = obtenerProductoPorId(id);
+        productos.remove(producto);
+    }
+
+    private int generarIdUnico() {
+        return productos.size() + 1;
+    }
 }
